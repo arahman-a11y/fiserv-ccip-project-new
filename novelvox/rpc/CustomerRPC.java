b0854f39cb04e00b71305974c44a7ed1f0c24ad7
Arshiyan Rahman <a.rahman@novelvox.com>
2026-06-10 17:37:32 +0530

    searchBySSN api

--- novelvox/rpc/CustomerRPC.java
+++ novelvox/rpc/CustomerRPC.java
@@ -21,8 +21,10 @@ public class CustomerRPC {
       public static void main(String[] args) {
         // Example usage
          System.out.println("Starting CustomerRPC main method");
-        SymXResponse response = CustomerRPC.searchByPhoneNumber("uniqueId123", "sequenceId456", "envId789", "11111");
-         System.out.println(response);
+        SymXResponse response = searchByPhoneNumber("uniqueId123", "sequenceId456", "envId789", "11111");
+        SymXResponse response2 = searchBySSN("uniqueId123", "sequenceId456", "envId789", "789-12-3456");
+         // System.out.println(response);
+         System.out.println(response2);
     }
   
 
@@ -44,5 +46,22 @@ public class CustomerRPC {
       }
    }
     
+        public static SymXResponse searchBySSN(String uniqueId, String sequenceId, String environmentId, String ssn) {
+      uniqueId = CustomisedFunctions.normalize(uniqueId);
+      sequenceId = CustomisedFunctions.normalize(sequenceId);
+      environmentId = CustomisedFunctions.normalize(environmentId);
+      ssn = CustomisedFunctions.normalize(ssn);
+      logger.info("{} searchBySSN - sequenceId: {}, ssn: {}", "INPUT|", sequenceId, ssn);
+      if (CommonGeneralFunctions.validateLicense()) {
+         String nullCheck = CommonGeneralFunctions.checkNulls("uniqueId", uniqueId, "sequenceId", sequenceId, "environmentId", environmentId, "ssn", ssn);
+         if (!nullCheck.equalsIgnoreCase("SUCCESS")) {
+            return new SymXResponse("400", "Bad Request", sequenceId, nullCheck);
+         } else {
+            return AESEncryption.validateKeys(uniqueId, "SymXData") && AESEncryption.validateKeys(environmentId, "SymXEnv") ? new SymXResponse("200", "SUCCESS", sequenceId, customerService.searchBySSN(ssn)) : new SymXResponse("401", "UNAUTHORIZED", sequenceId, " Issue in valdating UniqueId and EnvironmentId");
+         }
+      } else {
+         return new SymXResponse("403", "License Expired", sequenceId, " Licenses has been expired");
+      }
+   }
 
 }
