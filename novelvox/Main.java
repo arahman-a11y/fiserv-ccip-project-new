eae7dbe453c0206a1523cf002e9ec047dd81e6bc
Kanika Dhingra <dhingra.k@novelvox.com>
2026-06-10 16:15:49 +0530

    added main class

---
+++ novelvox/Main.java
@@ -0,0 +1,21 @@
+package novelvox;
+
+import java.util.Properties;
+
+public class Main {
+
+        public static void main(String[] args) throws Exception {
+
+
+    // Properties props = new Properties();
+
+    //     props.load(
+    //         new FileInputStream(
+    //             "property/SymXchange_CreditUnionData.properties"
+    //         )
+    //     );
+
+    //     System.out.println(props.getProperty("customers"));
+     System.out.println("Hello Fiserv");
+        }
+}
